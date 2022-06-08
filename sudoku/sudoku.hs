91aec5b09b2f26dcdd1e2653b8b10876baf7c208
JuniorNunes15 <juniornunesufc@gmail.com>
2022-06-08 09:02:05 -0300

    sudoku

---
+++ sudoku/sudoku.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+import Data.List
+import Data.Maybe
+import Data.Char
+
+neib :: [a] -> Int -> Int -> [a]
+--neib xs index lim = take (index+lim) $ drop (index-lim) xs 
+neib xs index lim
+                | index - lim <= 0 = take (index+lim+1) xs
+                | otherwise = take (index+lim) $ drop (index-lim) xs
+
+
+exists :: Eq a => a -> [a] -> Bool
+exists y xs = length (filter(==y) xs) == 1
+
+
+--dig2char :: (Eq a, Num a, Enum a) => a -> Char
+dig2char x = intToDigit x
+--dig2char x = toEnum x
+
+---fit :: (String, Int) ->  Int -> Int -> Bool
+--fit (xs, lim) index value = 
+
+
+getHoles :: String -> [Int]
+getHoles xs = getHoles' xs 0
+
+getHoles' [] _ = []
+getHoles' (x:xs) ind
+            | x == '.' = ind: getHoles' xs (ind+1)
+            | otherwise = getHoles' xs (ind+1)
+
+set (x:xs) 0 value = value : xs
+set (x:xs) index value = x : set xs (index-1) value
+
