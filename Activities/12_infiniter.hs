3d9d1a0e92971e79b2f3218eb12747416c5f5feb
JuniorNunes15 <juniornunesufc@gmail.com>
2022-06-28 15:17:06 -0300

    falta 2  itens para  terminar

--- Activities/12_infiniter.hs
+++ Activities/12_infiniter.hs
@@ -9,3 +9,24 @@ gerador1 = 0: get 1
             where
                 get x = [x,-x] ++ get (x+1)
 
+--gerador2
+
+gerador3 = get'' 1
+                where
+                    get'' cont = cont : get'' (cont+cont)
+
+
+expoentes x y
+            | mod x y == 0 = 1 + expoentes (div x y) y
+            | otherwise = 0
+
+
+--factors
+
+ 
+decompor x 
+        | x < 10 = [x]
+        | otherwise = decompor (div x 10) ++ [mod x 10] 
+
+
+--base
