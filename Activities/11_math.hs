d6a5c3655c1d2ee262d9dbae26ecf28f908be7c7
JuniorNunes15 <juniornunesufc@gmail.com>
2022-06-29 08:49:22 -0300

    falta 1 item

---
+++ Activities/11_math.hs
@@ -0,0 +1,14 @@
+
+
+sdig x 
+    | x < 10 = x
+    | otherwise = (mod x 10) +  sdig (div x 10)
+
+
+rev x = rev' x 0
+            where
+                rev' x cont
+                    | x < 10 = cont + x
+                    | otherwise = rev' (div x 10) ((cont+(mod x 10))*10)
+
+
