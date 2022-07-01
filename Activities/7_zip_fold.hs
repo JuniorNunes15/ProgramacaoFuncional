0b9d1cfb7100068744100d65bf189ca54c66d5af
JuniorNunes15 <juniornunesufc@gmail.com>
2022-07-01 17:22:44 -0300

    falta 3 itens

---
+++ Activities/7_zip_fold.hs
@@ -0,0 +1,45 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+
+--produto escalar
+produtoEscalar xs ys = calculo (zip xs ys)
+                where
+                    calculo [] = 0
+                    calculo (x:xs) = mult x + calculo xs
+                    mult (a,b) = a*b
+                                                
+
+--indices
+indices v xs = find $ zip xs [0..]
+                where
+                    find [] = []
+                    find (x:xs)
+                                | fst x == v = snd x : find xs
+                                | otherwise = find xs
+
+
+
+--concatMap
+
+
+--eh primo
+ehPrimo x 
+        | x == 2 || x == 3 || x == 5 || x == 7 || x == 11 = True
+        | foldl mod x [2,3,5,7,11] == 0 = False
+        | otherwise = True
+
+
+
+--vigenere
+
+
+
+--mudancaDeBase
+
+
+--reduce:
+divide (a,b) x
+                | mod a x == 0 && mod b x == 0 = divide (div a x, div b x) x
+                | otherwise = (a,b)
+
+reduce (a,b) = foldl divide (a,b) [11,7,5,3,2]
