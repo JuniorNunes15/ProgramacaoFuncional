578122e9e6c2d4804560ddf1ffacd6ac126f5911
JuniorNunes15 <juniornunesufc@gmail.com>
2022-07-10 14:13:45 -0300

    questoes math

--- Activities/11_math.hs
+++ Activities/11_math.hs
@@ -1,15 +1,23 @@
 
 
+--sdig soma dos digitos
 sdig x 
     | x < 10 = x
     | otherwise = (mod x 10) +  sdig (div x 10)
 
 
+--rev reverter inteiro
 rev x = rev' x 0
             where
                 rev' x cont
                     | x < 10 = cont + x
                     | otherwise = rev' (div x 10) ((cont+(mod x 10))*10)
 
---coverte a b
+
+--base mudanca de base
+base x y 
+        | x < y = [dx !! x]
+        | otherwise = base (div x y) y ++ [dx !! (mod x y)]
+            where
+                dx = ['0'..'9']++['A'..'Z'] 
 
