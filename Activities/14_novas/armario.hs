662f61001ad5d753ec9ad6a84beec8f108d021b1
JuniorNunes15 <juniornunesufc@gmail.com>
2022-07-10 11:29:14 -0300

    armarios completo

--- Activities/14_novas/armario.hs
+++ Activities/14_novas/armario.hs
@@ -9,30 +9,56 @@ type Armarios = Map.Map Int (Estado, Codigo)
 -- consulta : dado o número de um armário, retorna se ele está alugado ou não
 -- retorna Nothing se o armário não existir
 consulta :: Int -> Armarios -> Maybe Estado
-consulta indice armarios = ...
+consulta indice armarios =
+                    case Map.lookup indice armarios of
+                        Nothing -> Nothing
+                        Just (estado, codigo) -> if estado /= Alugado
+                                                then Just Livre
+                                                else Just Alugado
+
 
 -- dado o número do armário, retorna Right armário ou Left "Armario nao existe"
-pegarArmario :: Int -> Armarios -> Either String Armario
-pegarArmario indice armarios = ...
+pegarArmario :: Int -> Armarios -> Either String Armarios
+pegarArmario indice armarios = 
+                    case Map.lookup indice armarios of
+                        Nothing -> Left $ "Armario " ++ show indice ++ " nao existe"
+                        Just (estado, codigo) -> Right $ armarios --if estado /= Alugado
+                                                --then Right armarios
+                                                --else Left "asd"
 
 -- consultar: dado o número de um armário retorna "Right código"
 -- Se não existir, retorna "Left Armario não existe", se já estiver alugado retorna "Left ja esta ocupado"
 -- Utilize a função pegarArmario para obter o armário e verificar se ele existe
 -- Não reescreva a mesma mensagem de erro, utilize a invocação do pegarArmário
 pegarCodigo :: Int -> Armarios -> Either String Codigo
-pegarCodigo indice armarios = ...
+pegarCodigo indice armarios = 
+                    case Map.lookup indice armarios of
+                        Nothing -> Left $ "Armario " ++ show indice ++ " nao existe"
+                        Just (estado, codigo) -> if estado /= Alugado
+                                                then Right codigo
+                                                else Left $ "armario " ++ show indice ++ " ja esta ocupado"
 
 
 -- aluguel - se o armário existir e estiver desocupado, o estado do armário no mapa é alterado para alugado
 -- reaproveite a função pegarCodigo para obter a chave atual e os erros de "não existe" e "está ocupado"
 aluguel :: Int -> Armarios -> Either String Armarios
-aluguel indice armarios = ...
+aluguel indice armarios = 
+                    case Map.lookup indice armarios of
+                        Nothing -> Left $ "nao existe"
+                        Just (estado, codigo) -> if estado /= Livre
+                                                then Left $ "Armario " ++ show indice ++ " ja esta ocupado"
+                                                else Right $ Map.insert indice (Alugado, codigo) armarios  --[(indice, (Livre, codigo))] --fromList [(indice,(Alugado,codigo))]
 
 -- para devolver um armário é necessário que ele exista, que esteja alugado e que o código esteja correto
 -- utilize a função pegar armário para verificar a existência.
 -- Se o armário não estiver alugado ou se o código estiver incorreto avise utilizando o Left.
 devolucao :: Int -> Codigo -> Armarios -> Either String Armarios
-devolucao indice codigo armarios =  ...
+devolucao indice cod armarios = 
+                        case Map.lookup indice armarios of
+                            Nothing -> Left $ "Armario " ++ show indice ++ " nao existe"
+                            Just (estado, codigo) -> if estado == Alugado && cod == codigo
+                                                    then Right $ Map.insert indice (Livre, codigo) (Map.delete indice armarios) --armarios--fromList [(indice,(Livre,codigo))]
+                                                    else Left $ "codigo incorreto"
 
 
 lockers :: Armarios
